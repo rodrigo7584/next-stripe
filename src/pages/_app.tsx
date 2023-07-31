e559b2e74838f9dff5ffd0a522a0dbbfc941f1db
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-07-30 21:13:01 -0300

    30/07/2023

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
-export default MyApp
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
+
+import Image from 'next/image';
+
+globalStyles();
+
+export default function App({ Component, pageProps }:AppProps) {
+  return (
+  <div> 
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+        <Component {...pageProps} />  
+    </Container>
+  </div>
+)}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { styled } from "../styles"
+
+const Button = styled('button',{
+  backgroundColor: '$green500',
+  borderRadius: 2,
+  border: 'none',
+  padding: '4px 8px',
+  color: '#ffffff',
+
+  '&:hover':{
+    filter:'brightness(0.9)',
+    cursor: 'pointer'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>
+    <Button>
       Hello 
-    </h1>
+    </Button>
   )
 }
